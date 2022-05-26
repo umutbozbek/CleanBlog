23fb5d6cf63a75f3f7c86408c88ed9230b395141
umutbozbek <umutbozbek58@gmail.com>
2022-05-26 23:52:04 +0300

    clean-blog-projesi-2.bölüm

--- app.js
+++ app.js
@@ -1,14 +1,34 @@
 const  express =require('express')
+const path=require('path')
+const ejs=require('ejs')
+
 
 const app=express();
 
+app.set('view engine', 'ejs');
 
-app.get('/',(req,res)=>{
-    const blog = { id: 1, title: "Blog title", description: "Blog description" }
 
-    res.send(blog)
-})
 
+//MİDDLEWARES
+app.use(express.static(path.resolve(__dirname + '/public')));
+
+
+//Rautes
+app.get('/', (req, res) => {
+    res.render('index');
+  });
+  
+  app.get('/about', (req, res) => {
+    res.render('about');
+  });
+  
+  app.get('/add_post', (req, res) => {
+    res.render('add_post');
+  });
+  
+  app.get('/post', (req, res) => {
+    res.render('post');
+  });
 
 const port = 5000
 
